7ced97632f16ee4fc8f084341a8936ae5f6625ed
Angelo <angelo@rcstart.it>
2021-09-18 19:05:31 +0200

    Add: add a new example (get_tx)

---
+++ examples/node/get_tx.ts
@@ -0,0 +1,13 @@
+import { BitsongClient } from '../../build';
+
+const rpcUrl = 'http://49.12.198.197:26657';
+
+(async () => {
+    const bitsong = await BitsongClient.connect(rpcUrl);
+
+    const tx = await bitsong.getTx('5685C931FEEC799EB406920E6C2E6F0359070B192CEEFA0B78515CCA65087BB3');
+    console.log(tx);
+
+    // rawLog
+    console.log(tx.rawLog);
+})();
