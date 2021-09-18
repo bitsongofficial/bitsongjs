d2ce68409b616cc7c05ead3c9f9470f2203d30f7
Angelo <angelo@rcstart.it>
2021-09-18 12:25:07 +0200

    add some node examples

---
+++ examples/node/create_wallet.ts
@@ -0,0 +1,16 @@
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+import { stringToPath } from '@cosmjs/crypto';
+// eslint-disable-next-line node/no-unpublished-import
+import { Constants } from '../../build/build';
+
+const createWallet = async () => {
+    return await DirectSecp256k1HdWallet.generate(12, { prefix: Constants.Bech32PrefixAccAddr, hdPaths: [stringToPath(Constants.getHdPath())] });
+};
+
+(async () => {
+    const wallet = await createWallet();
+    const [account] = await wallet.getAccounts();
+
+    console.log(`Mnemonic: ${wallet.mnemonic}`);
+    console.log(`Address: ${account.address}`);
+})();
