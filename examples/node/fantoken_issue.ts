2343638bb1bd24234809e39c3b44bc7d4dfb16b6
Angelo <angelo@rcstart.it>
2022-08-27 14:25:36 +0200

    examples fixs

--- examples/node/fantoken_issue.ts
+++ examples/node/fantoken_issue.ts
@@ -1,53 +1,80 @@
-import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+import { DirectSecp256k1HdWallet, decodeTxRaw } from '@cosmjs/proto-signing';
 import { stringToPath } from '@cosmjs/crypto';
 // eslint-disable-next-line node/no-unpublished-import
 import { Constants, BitsongClient, SigningBitsongClient } from '../../packages/sdk/build';
-import { assertIsBroadcastTxSuccess } from '@cosmjs/stargate';
+import { assertIsDeliverTxSuccess, logs } from '@cosmjs/stargate';
+import { MsgIssueEncodeObject } from '../../packages/sdk/build/messages';
+import { MsgIssue, MsgMint } from '../../packages/sdk/build/codec/bitsong/fantoken/v1beta1/tx';
+import { MsgMintEncodeObject } from 'packages/sdk/src/messages';
 
-const rpcUrl = 'http://49.12.198.197:26657';
+const rpcUrl = 'https://rpc.nft-devnet.bitsong.network/';
 
 const user = {
-    mnemonic: 'usage elegant version gold snake soap check maze chapter harsh put common',
-    address: 'bitsong1qyglp2h28cq7eunmpusd4ngrfdsp0zjf3l9fz5',
+  mnemonic: 'vessel hint uphold attitude piece prison fold staff cattle urban net chuckle',
+  address: 'bitsong1fqhdc4lruy4l2pv25lnn8f8k6app8w9gkf4c6a',
 };
 
 const defaultFee = {
-    amount: [
-        {
-            denom: Constants.MicroDenom,
-            amount: '2000',
-        },
-    ],
-    gas: '180000', // 180k
+  amount: [
+    {
+      denom: Constants.MicroDenom,
+      amount: '2000',
+    },
+  ],
+  gas: '180000', // 180k
 };
 
 (async () => {
-    // Set Wallet
-    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(user.mnemonic, { prefix: Constants.Bech32PrefixAccAddr, hdPaths: [stringToPath(Constants.getHdPath())] });
-    const [account] = await wallet.getAccounts();
+  // Set Wallet
+  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(user.mnemonic, { prefix: Constants.Bech32PrefixAccAddr, hdPaths: [stringToPath(Constants.getHdPath())] });
+  const [account] = await wallet.getAccounts();
 
-    console.log(`Mnemonic: ${wallet.mnemonic}`);
-    console.log(`Address: ${account.address}`);
+  console.log(`Mnemonic: ${wallet.mnemonic}`);
+  console.log(`Address: ${account.address}`);
 
-    // Init the client
-    const bitsong = await BitsongClient.connect(rpcUrl);
-    const signingBitsong = await SigningBitsongClient.connectWithSigner(rpcUrl, wallet);
+  // Init the client
+  const signingBitsong = await SigningBitsongClient.connectWithSigner(rpcUrl, wallet);
 
-    console.log('issuing a new fantoken denom...');
+  console.log('issuing a new fantoken denom...');
 
-    const symbol = 'test22';
-    const name = 'Test token';
-    const description = 'Test description fantoken';
-    const maxSupply = '100000000000'; // 100k
-    const issueFee = {
-        denom: Constants.MicroDenom,
-        amount: '1000000',
-    };
+  const msg: MsgIssueEncodeObject = {
+    typeUrl: '/bitsong.fantoken.MsgIssue',
+    value: MsgIssue.fromPartial({
+      authority: account.address,
+      maxSupply: "100000000000", // 100.000
+      minter: account.address,
+      name: "fantoken test",
+      symbol: "test",
+      uri: "ipfs://",
+    }),
+  };
 
-    const result = await signingBitsong.issueFanToken(symbol, name, maxSupply, description, account.address, issueFee, defaultFee, 'my first fantoken');
-    assertIsBroadcastTxSuccess(result);
-    console.log(`Tx Hash: ${result.transactionHash}`);
+  let result = await signingBitsong.signAndBroadcast(account.address, [msg], defaultFee, "test MsgIssue")
+  assertIsDeliverTxSuccess(result);
+  console.log(`Tx Hash: ${result.transactionHash}`);
 
-    const balance = await bitsong.getBalance(account.address, 'utest22');
-    console.log(`Your new balance is: ${balance.amount}${balance.denom}`);
+  const parsedLogs = logs.parseLogs(logs.parseRawLog(result.rawLog));
+  const denomAttr = logs.findAttribute(parsedLogs, "bitsong.fantoken.v1beta1.EventIssue", "denom");
+  const denom = denomAttr.value.slice(1, -1)
+
+  console.log('minting a new fantoken denom...');
+
+  const msgMint: MsgMintEncodeObject = {
+    typeUrl: '/bitsong.fantoken.MsgMint',
+    value: MsgMint.fromPartial({
+      recipient: account.address,
+      coin: {
+        amount: "10",
+        denom: denom
+      },
+      minter: account.address
+    }),
+  };
+
+  result = await signingBitsong.signAndBroadcast(account.address, [msgMint], defaultFee, "test MsgMint")
+  assertIsDeliverTxSuccess(result);
+  console.log(`Tx Hash: ${result.transactionHash}`);
+
+  /*const balance = await bitsong.getBalance(account.address, 'utest22');
+  console.log(`Your new balance is: ${balance.amount}${balance.denom}`);*/
 })();
--- examples/node/send_coin.ts
+++ examples/node/send_coin.ts
@@ -2,58 +2,58 @@ import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
 import { stringToPath } from '@cosmjs/crypto';
 // eslint-disable-next-line node/no-unpublished-import
 import { Constants, BitsongClient, SigningBitsongClient } from '../../packages/sdk/build';
-import { assertIsBroadcastTxSuccess } from '@cosmjs/stargate';
+import { assertIsDeliverTxSuccess } from '@cosmjs/stargate';
 
-const rpcUrl = 'http://49.12.198.197:26657';
+const rpcUrl = 'https://rpc.nft-devnet.bitsong.network/';
 
 const faucet = {
-    mnemonic: 'fuel obscure melt april direct second usual hair leave hobby beef bacon solid drum used law mercy worry fat super must ritual bring faculty',
-    address: 'bitsong1nzxmsks45e55d5edj4mcd08u8dycaxq5eplakw',
+  mnemonic: 'friend excite rough reopen cover wheel spoon convince island path clean monkey play snow number walnut pull lock shoot hurry dream divide concert discover',
+  address: 'bitsong1zm6wlhr622yr9d7hh4t70acdfg6c32kcv34duw',
 };
 
 const createWallet = async () => {
-    return await DirectSecp256k1HdWallet.generate(12, { prefix: Constants.Bech32PrefixAccAddr, hdPaths: [stringToPath(Constants.getHdPath())] });
+  return await DirectSecp256k1HdWallet.generate(12, { prefix: Constants.Bech32PrefixAccAddr, hdPaths: [stringToPath(Constants.getHdPath())] });
 };
 
 const defaultFee = {
-    amount: [
-        {
-            denom: Constants.MicroDenom,
-            amount: '2000',
-        },
-    ],
-    gas: '180000', // 180k
+  amount: [
+    {
+      denom: Constants.MicroDenom,
+      amount: '2000',
+    },
+  ],
+  gas: '180000', // 180k
 };
 
 (async () => {
-    // Faucet Wallet
-    const faucetWallet = await DirectSecp256k1HdWallet.fromMnemonic(faucet.mnemonic, { prefix: Constants.Bech32PrefixAccAddr, hdPaths: [stringToPath(Constants.getHdPath())] });
-    // Create wallet
-    const wallet = await createWallet();
-    const [account] = await wallet.getAccounts();
+  // Faucet Wallet
+  const faucetWallet = await DirectSecp256k1HdWallet.fromMnemonic(faucet.mnemonic, { prefix: Constants.Bech32PrefixAccAddr, hdPaths: [stringToPath(Constants.getHdPath())] });
+  // Create wallet
+  const wallet = await createWallet();
+  const [account] = await wallet.getAccounts();
 
-    console.log(`Mnemonic: ${wallet.mnemonic}`);
-    console.log(`Address: ${account.address}`);
+  console.log(`Mnemonic: ${wallet.mnemonic}`);
+  console.log(`Address: ${account.address}`);
 
-    // Init the client
-    const bitsong = await BitsongClient.connect(rpcUrl);
-    const signingBitsong = await SigningBitsongClient.connectWithSigner(rpcUrl, faucetWallet);
+  // Init the client
+  const bitsong = await BitsongClient.connect(rpcUrl);
+  const signingBitsong = await SigningBitsongClient.connectWithSigner(rpcUrl, faucetWallet);
 
-    let balance = await bitsong.getBalance(account.address, Constants.MicroDenom);
-    console.log(`Your actual balance is: ${balance.amount}${balance.denom}`);
+  let balance = await bitsong.getBalance(account.address, Constants.MicroDenom);
+  console.log(`Your actual balance is: ${balance.amount}${balance.denom}`);
 
-    // Send coins from faucet to the new account
-    const amount = {
-        denom: Constants.MicroDenom,
-        amount: '10000000', // 10btsg
-    };
+  // Send coins from faucet to the new account
+  const amount = {
+    denom: Constants.MicroDenom,
+    amount: '100000000000', // 100.000btsg
+  };
 
-    console.log('sending tokens...');
-    const result = await signingBitsong.sendTokens(faucet.address, account.address, [amount], defaultFee, 'Have fun!');
+  console.log('sending tokens...');
+  const result = await signingBitsong.sendTokens(faucet.address, account.address, [amount], defaultFee, 'Have fun!');
 
-    assertIsBroadcastTxSuccess(result);
-    console.log(`Tx Hash: ${result.transactionHash}`);
+  assertIsDeliverTxSuccess(result);
+  console.log(`Tx Hash: ${result.transactionHash}`);
 
-    balance = await bitsong.getBalance(account.address, Constants.MicroDenom);
-    console.log(`Your new balance is: ${balance.amount}${balance.denom}`);
+  balance = await bitsong.getBalance(account.address, Constants.MicroDenom);
+  console.log(`Your new balance is: ${balance.amount}${balance.denom}`);
 })();
