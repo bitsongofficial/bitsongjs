c8cb9e1294bc92d360392e6cce37872c44df2347
DavideSegullo <davidesegullo@gmail.com>
2022-10-14 15:54:30 +0200

    feat: :sparkles: add contracts package for cosmwasm ts codegen

---
+++ packages/contracts/scripts/codegen.js
@@ -0,0 +1,39 @@
+const { join, resolve } = require('path');
+const { default: codegen } = require('@cosmwasm/ts-codegen');
+
+const contractsDir = resolve(join(__dirname, '/../schemas'));
+
+const contracts = [
+	{
+		name: 'CW721Base',
+		dir: join(contractsDir, 'cw721-base'),
+	},
+];
+
+codegen({
+	contracts,
+	outPath: join(__dirname, '../src/codegen'),
+	options: {
+		bundle: {
+			enabled: true,
+			bundleFile: 'index.ts',
+			scope: 'contracts',
+		},
+		types: {
+			enabled: true,
+		},
+		client: {
+			enabled: true,
+		},
+		messageComposer: {
+			enabled: true,
+		},
+	},
+})
+	.then(() => {
+		console.log('✨ all done!');
+	})
+	.catch(e => {
+		console.error(e);
+		process.exit(1);
+	});
