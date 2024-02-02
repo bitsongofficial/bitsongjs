9807f212f0b0d5755e2d7c2eb0b6ff31bef74940
angelorc <angelo@rcstart.it>
2024-02-02 10:58:50 +0100

    fix jest config

--- packages/metadata/jest.config.js
+++ packages/metadata/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  moduleNameMapper: {
+    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+    uuid: require.resolve('uuid'),
+  },
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
