d0caf17a9566fa6bc488e2ce28ec1283fcde0972
angelorc <angelo@rcstart.it>
2024-11-02 01:03:42 +0100

    fix: should fix an issue with recursive types in ts

--- packages/metadata/src/utils.ts
+++ packages/metadata/src/utils.ts
@@ -6,12 +6,15 @@ export type Brand<T, TBrand, ReservedName extends string = '__type__'> = T & {
 
 type BrandOf<A> = [A] extends [Brand<unknown, infer R>] ? R : never;
 
-export type RecursiveUnbrand<T> = T extends Brand<infer R, BrandOf<T>>
-  ? R
-  : {
-    [K in keyof T]: RecursiveUnbrand<T[K]>;
-  };
+type UnbrandSingle<T> = T extends Brand<infer R, BrandOf<T>> ? R : T;
 
+export type RecursiveUnbrand<T> = UnbrandSingle<T> extends infer U
+  ? U extends object
+  ? {
+    [K in keyof U]: RecursiveUnbrand<U[K]>;
+  }
+  : U
+  : never;
 
 export type ShapeCheck<T> = T extends {
   $schema: string;
