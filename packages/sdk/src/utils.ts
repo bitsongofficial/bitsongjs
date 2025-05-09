8219d2b9d50309970ddff0148ec2ca1470a9958d
angelorc <angelo@rcstart.it>
2025-05-09 15:32:15 +0200

    feat(sdk): update SDK version to 2.0.0-dev.11 and add `experimental_simulate` method

--- packages/sdk/src/utils.ts
+++ packages/sdk/src/utils.ts
@@ -1,4 +1,5 @@
-import { Bip39, Random, Slip10RawIndex } from '@cosmjs/crypto';
+import { Secp256k1HdWallet } from '@cosmjs/amino';
+import { Bip39, Random, Slip10RawIndex, stringToPath } from '@cosmjs/crypto';
 
 export function makeHdPath(coinType = 118, account = 0) {
   return [
@@ -21,6 +22,13 @@ export function generateEntropy(length: 12 | 15 | 18 | 21 | 24 = 12): Uint8Array
 
 export function generateMnemonic(length: 12 | 15 | 18 | 21 | 24 = 12): string {
   return Bip39.encode(generateEntropy(length)).toString();
+}
+
+export async function generateWallet(): Promise<Secp256k1HdWallet> {
+  return Secp256k1HdWallet.fromMnemonic(Bip39.encode(generateEntropy()).toString(), {
+    prefix: 'bitsong',
+    hdPaths: [stringToPath(`m/44'/639'/0'/0/0`)],
+  })
 }
 
 export { 
